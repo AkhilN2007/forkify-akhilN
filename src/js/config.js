5edf9b0c757d30d93ccd0e6384c077b3820b3ab5
AkhilN2007 <akhil26.nandamuri@gmail.com>
2022-09-10 11:40:43 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '60b6c8c2-20db-47a1-bbad-067d2b81f09a';
+export const MODAL_CLOSE_SEC = 2.5;
